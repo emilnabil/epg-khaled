6e6ef1570e77117b12c5e530727cc4e25b1f88ec
emil-nabil333 <elbrinselomda@yahoo.com>
2022-07-26 13:22:48 -0700

    Add files via upload

---
+++ EPG-khaled.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+
+wget -O /etc/epgimport/khaled.epg.sources.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled.epg.sources.xml"
+wget -O /etc/epgimport/khaled.epg.channels.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled.epg.channels.xml"
+wget -O /etc/epgimport/khaled2.epg.channels.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled2.epg.channels.xml"
+wget -O /etc/epgimport/khaled3.epg.channels.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled3.epg.channels.xml"
+wget -O /etc/epgimport/khaled4.epg.channels.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled4.epg.channels.xml"
+wget -O /etc/epgimport/khaled5.epg.channels.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled5.epg.channels.xml"
+wget -O /etc/epgimport/khaled6.epg.channels.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled6.epg.channels.xml"
+wget -O /etc/epgimport/ar_osn.xmltv "https://raw.githubusercontent.com/Fazzani/grab/master/ar_osn.xmltv"
+wget -O /etc/epgimport/ar_osn2.xmltv "https://raw.githubusercontent.com/Fazzani/grab/master/ar_osn2.xmltv"
+wget -O /etc/epgimport/ar_osn3.xmltv "https://raw.githubusercontent.com/Fazzani/grab/master/ar_osn3.xmltv"
+wget -O /etc/epgimport/ar_osn4.xmltv "https://raw.githubusercontent.com/Fazzani/grab/master/ar_osn4.xmltv"
+wget -O /etc/epgimport/ar_osn5.xmltv "https://raw.githubusercontent.com/Fazzani/grab/master/ar_osn5.xmltv"
+wget -O /etc/epgimport/ar_arab.xmltv "https://raw.githubusercontent.com/Fazzani/grab/master/ar_arab.xmltv"
+wget -O /etc/epgimport/ar_el_cinema.xmltv "https://raw.githubusercontent.com/Fazzani/grab/master/ar_el_cinema.xmltv"
+wget -O /etc/epgimport/ar_bein.xmltv "https://raw.githubusercontent.com/Fazzani/grab/master/ar_bein.xmltv"
+wget -O /etc/epgimport/jawwytv.xml "https://raw.githubusercontent.com/MOHAMED19OS/XMLTV/main/jawwytv.xml"
+wget -O /etc/epgimport/khaled.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled.xml"
+wget -O /etc/epgimport/khaled1.xml "https://raw.githubusercontent.com/emilnabil/epg-khaled/main/khaled1.xml"
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
